5a166bb8d94985b0c47459b643fd5053aadb38ad
Ian Auld <imauld@gmail.com>
2014-06-17 09:28:24 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,3 @@ SECRET_KEY = '\xd0\x9bV\x06w 6\x82z\\W\xa2l\xd6L\x99\xbek^ \xcb\xfb\xcc2'
 MAIL_SERVER = "smtp.gmail.com"
 MAIL_PORT = 465
 MAIL_USE_SSL = True
-MAIL_USERNAME = 'imauld@gmail.com'
-MAIL_PASSWORD = 'LikeABoss85!@58'
