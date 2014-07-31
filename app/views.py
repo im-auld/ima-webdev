d3fcd87f59f0a5323bec326d70424e6cb7e871fc
Ian Auld <imauld@gmail.com>
2014-07-31 07:01:34 -0700

    Initial site redesign

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app, mail
-from flask import render_template, request, redirect, url_for
+from flask import render_template, redirect, url_for
 from flask.ext.mail import Message
 from forms import ContactForm
 
@@ -9,26 +9,31 @@ from forms import ContactForm
 def index():
     return render_template('index.html')
 
-    
+
 @app.route('/about/')
 def about():
     return render_template('about.html')
 
-    
+
 @app.route('/contact/', methods = ['GET', 'POST'])
 def contact():
     form = ContactForm()
     if form.validate_on_submit():
         print u'Validated form'
         subject = form.subject.data
-        body = """
-                Message from {name} ({email}),
-                
-                {message}
-                """.format(name=form.name.data, email=form.email.data, message=form.message.data)
+        body = """Message from {name} ({email}),\n\n{message}""".format(
+            name=form.name.data,
+            email=form.email.data,
+            message=form.message.data
+        )
         sender = (form.name.data, form.email.data)
         recipients = ['imauld@gmail.com']
-        msg = Message(subject=subject, body=body, sender=sender, recipients=recipients)
+        msg = Message(
+            subject=subject,
+            body=body,
+            sender=sender,
+            recipients=recipients
+        )
         mail.send(msg)
         return redirect(url_for('contact'))
     else:
