8bbcfbd145c4f9dd04cd419789e3d46f35a393f3
Ian Auld <imauld@gmail.com>
2014-08-07 12:54:55 -0400

    Changed to config to keep secret stuff secret

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+import os
+
+
 CSRF_ENABLED = True
-SECRET_KEY = '\xd0\x9bV\x06w 6\x82z\\W\xa2l\xd6L\x99\xbek^ \xcb\xfb\xcc2'
+SECRET_KEY = os.urandom(32)
 
 MAIL_SERVER = "smtp.gmail.com"
 MAIL_PORT = 465
 MAIL_USE_SSL = True
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'ian@example.com')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'password')
